a42563b3d8ed35a2f1f749b8ab8389123eac18eb
Anurag Ohri <anuragohri92@gmail.com>
2022-01-31 11:44:22 +0530

    Removed android config

--- release.nix
+++ release.nix
@@ -7,11 +7,11 @@ in {
   #     system = "x86_64-darwin";
   #   };
   # }).ios.frontend;
-  android = (import ./. {
-    obelisk = import ./.obelisk/impl {
-      config.android_sdk.accept_license = true;
-    };
-  }).android.frontend;
+  # android = (import ./. {
+  #   obelisk = import ./.obelisk/impl {
+  #     config.android_sdk.accept_license = true;
+  #   };
+  # }).android.frontend;
   exe = local-self.exe;
   server = local-self.server;
   linuxExe = local-self.linuxExe;
